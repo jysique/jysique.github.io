61f9368e6fe35fd5c7ae4a8f2b88e966f5cdcfa7
Jose Ysique <joseysique@gmail.com>
2022-10-18 16:46:50 -0500

    update

---
+++ js/components/TaskApp.js
@@ -0,0 +1,11 @@
+
+window.taskApp = () => {
+    return {
+        tasks: [],
+        newTask: "",
+        submit() {
+            this.tasks.push({ body: this.newTask, completed: false });
+            this.newTask = ''
+        }
+    }
+}
